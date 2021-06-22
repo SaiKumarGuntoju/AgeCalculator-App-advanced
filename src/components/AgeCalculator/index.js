7ec310423308a2e1b4dfd1435fd6104bb1cf8461
SaiKumarGuntoju <saikumarguntoju321@gmail.com>
2021-06-22 03:53:07 +0000

    AgeCalculator v1

---
+++ src/components/AgeCalculator/index.js
@@ -0,0 +1,136 @@
+// Write your code here.
+import {Component} from 'react'
+
+import './index.css'
+
+class AgeCalculator extends Component {
+  state = {
+    yearOfBirth: '',
+    showError: false,
+    showResult: false,
+  }
+
+  setIsResultVisible = value => {
+    this.setState({showResult: value})
+  }
+
+  setIsErrorOccurred = value => {
+    this.setState({showError: value})
+  }
+
+  renderErrorMessage = () => {
+    const {showError} = this.state
+
+    if (showError) {
+      return <p className="error-message">Enter the year that you are Born</p>
+    }
+    return null
+  }
+
+  getCalculatedAge = () => {
+    const {yearOfBirth} = this.state
+    const dateOfBirth = new Date(yearOfBirth)
+    const dateOdBirthYear = dateOfBirth.getFullYear()
+
+    const presentDate = new Date()
+    const presentDateYear = presentDate.getFullYear()
+
+    return presentDateYear - dateOdBirthYear
+  }
+
+  onClickAgeCalculate = () => {
+    const {yearOfBirth} = this.state
+    const age = this.getCalculatedAge()
+
+    if (age > 0 && yearOfBirth.length <= 4 && Number.isInteger(age)) {
+      this.setIsErrorOccurred(false)
+      this.setIsResultVisible(true)
+    } else {
+      this.setIsErrorOccurred(true)
+    }
+  }
+
+  renderCalculateButton = () => (
+    <div className="button-container">
+      <button
+        className="calculate-button"
+        onClick={this.onClickAgeCalculate}
+        type="button"
+      >
+        Calculate
+      </button>
+    </div>
+  )
+
+  getCalculatedAgeText = () => {
+    const calculatedAge = this.getCalculatedAge()
+
+    if (calculatedAge === 1) {
+      return `You are ${calculatedAge} year old by the end of 2021`
+    }
+    return `You are ${calculatedAge} years old by the end of 2021`
+  }
+
+  onChangeYearOfBirth = event => {
+    const {value} = event.target
+
+    this.setState({yearOfBirth: value})
+    this.setIsResultVisible(false)
+    this.setIsErrorOccurred(false)
+  }
+
+  renderCalculatedAge = () => {
+    const {showResult} = this.state
+
+    if (showResult) {
+      return (
+        <p className="calculated-age-text">{this.getCalculatedAgeText()}</p>
+      )
+    }
+    return null
+  }
+
+  renderInputField = () => {
+    const {yearOfBirth} = this.state
+    return (
+      <input
+        className="input-field"
+        onChange={this.onChangeYearOfBirth}
+        placeholder="Enter the year that you are born"
+        type="text"
+        value={yearOfBirth}
+      />
+    )
+  }
+
+  renderAgeCalculatorCard = () => (
+    <div className="age-calculator-card">
+      <h1>Age Calculator</h1>
+      <div className="form-container">
+        <div className="input-with-error-container">
+          {this.renderInputField()}
+          {this.renderErrorMessage()}
+        </div>
+        {this.renderCalculatedAge()}
+        {this.renderCalculateButton()}
+      </div>
+    </div>
+  )
+
+  render() {
+    return (
+      <div className="age-calculator-container">
+        {this.renderAgeCalculatorCard()}
+        <div className="image-container">
+          <img
+            alt="stages of human"
+            className="age-calculator-image"
+            src="https://assets.ccbp.in/frontend/react-js/age-calculater-persons-img.png"
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default AgeCalculator
